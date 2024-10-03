cb7959141ce93b91be142129f8ff35b76e067943
lukachochua <91949321+lukachochua@users.noreply.github.com>
2024-10-03 12:21:57 +0400

    chore: naming changes

--- config/adminlte.php
+++ config/adminlte.php
@@ -257,7 +257,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'home',
+    'dashboard_url' => 'dashboard',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
@@ -315,13 +315,13 @@ return [
             'url' => 'admin/blog',
             'can' => 'manage-blog',
         ],
-        [
-            'text' => 'pages',
-            'url' => 'admin/pages',
-            'icon' => 'far fa-fw fa-file',
-            'label' => 4,
-            'label_color' => 'success',
-        ],
+        // [
+        //     'text' => 'pages',
+        //     'url' => 'admin/pages',
+        //     'icon' => 'far fa-fw fa-file',
+        //     'label' => 4,
+        //     'label_color' => 'success',
+        // ],
         ['header' => 'account_settings'],
         [
             'text' => 'profile',
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,12 +3,22 @@
 
 @section('content')
     <div class="container">
-        <h2>Welcome to your dashboard, {{ Auth::user()->name }}</h2>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card mt-5">
+                    <div class="card-header">
+                        @if (session('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                        @endif
 
-        @if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
+                        <div class="card card-primary shadow-lg mt-3 bg-secondary">
+                            <h2 class="text-center">Welcome to your dashboard, {{ Auth::user()->name }}</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
-        @endif
+        </div>
     </div>
 @endsection
